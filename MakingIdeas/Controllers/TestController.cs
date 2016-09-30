826fc92be9a6732bb75a2307a904325348d5bc4d
Paweł Maga <pawel.p.maga@gmail.com>
2016-09-30 11:34:38 +0200

    removed redeuntant stuff, added testcontroller

---
+++ MakingIdeas/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MakingIdeas.Controllers
+{
+    [RoutePrefix("api/Test")]
+    public class TestController : ApiController
+    {
+        [System.Web.Mvc.HttpGet]
+        [Route("GetWelcomeMessage")]
+        public string GetWelcomeMessage()
+        {
+            return "Welcome from Web API";
+        }
+    }
+}
